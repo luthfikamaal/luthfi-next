117d78924f08be97669cb038a9909d78f0873369
luthfikamaal <luthfikamal13@gmail.com>
2023-01-04 07:09:51 +0700

    add meta tag lagi

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,10 @@ export default function Home() {
     <>
       <Head>
         <title>Home - Luthfi's Hideout</title>
+        <meta property="og:title" content="Luthfi's Hideout" />
+        <meta property="og:description" content="Tempat persembunyianku" />
+        <meta property="og:type" content="Personal Web" />
+        <meta property="og:image" content="https://avatars.githubusercontent.com/u/90312645?v=4" />
       </Head>
       <Navbar />
       <div className="content pt-20">
